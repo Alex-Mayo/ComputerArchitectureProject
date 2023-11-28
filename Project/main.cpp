31e5d25b136abb43a6adad7d0b5570e9cd634490
Alex-Mayo <alexander.mayo@mymail.champlain.edu>
2023-11-28 10:56:47 -0500

    Create card object

--- Project/main.cpp
+++ Project/main.cpp
@@ -1,4 +1,28 @@
 extern "C" void  asmMain();
+#include<iostream>
+
+
+using namespace std;
+
+class Card{
+
+   public:
+      void setName(string temp){name = temp;}
+      void setNum(int temp){number = temp;}
+      void setSuit(string temp){suit = temp;}
+
+      string getName(){return name;}
+      int getNum(){return number;}
+      string getSuit(){return suit;}
+
+   private:
+      string name;
+      int number;
+      string suit;
+};
+
+
+
 
 int main(){
    asmMain();
