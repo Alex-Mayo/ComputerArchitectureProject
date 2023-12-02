41cc8200f5ac64fc55a51662fcbe4fe447e1fc81
jackyzhang <jacky.zhang@mymail.champlain.edu>
2023-12-01 22:37:33 -0500

    Changed the skeleton code to make sure that it just runs.testing to make sure git works

--- Project/asmMain.asm
+++ Project/asmMain.asm
@@ -1,19 +1,14 @@
-
-
 .data
 
 card STRUCT
-   name DB 256 ?
+   name DB 256 DUP(?)  ; Use DUP to allocate an array of 256 bytes
    number DB ?
-   suit DB 128 ?
-   
+   suit DB 128 DUP(?) ; Use DUP to allocate an array of 128 bytes
 card ENDS
-
-
-
 
 .CODE
 asmMain PROC
+    ; Your code for asmMain goes here
 
-asmMain END
+asmMain ENDP
 END
