55ee2581a379649f254ec6322b78e4865a4408d3
jackyzhang <jacky.zhang@mymail.champlain.edu>
2023-12-04 15:01:15 -0500

    Add some structure to main.

--- Project/main.cpp
+++ Project/main.cpp
@@ -1,12 +1,25 @@
-extern "C" void asmMain();
-#include<iostream>
+#include <iostream>
+#include <cstdlib>
+
+extern "C" {
+    void asmMain(); // Declare the assembly function
+}
 
 using namespace std;
 
-
-
-
 int main() {
-	asmMain();
-	return 0;
+    char playAgain;
+
+    do {
+        cout << "Welcome to Blackjack!\n";
+        asmMain(); // Call the assembly function
+
+        // Ask the user if they want to play again
+        cout << "Do you want to play again? (y/n): ";
+        cin >> playAgain;
+
+    } while (playAgain == 'y' || playAgain == 'Y');
+
+    cout << "Thanks for playing!\n";
+    return 0;
 }
