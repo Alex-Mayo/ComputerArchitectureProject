8c9e3a4e31e41e040ca45d1ec08e096e06603ae6
Alex-Mayo <alexander.mayo@mymail.champlain.edu>
2023-12-05 10:20:20 -0500

    Bringing code inline with other branch

--- Project/main.cpp
+++ Project/main.cpp
@@ -1,12 +1,29 @@
-extern "C" void asmMain();
-#include<iostream>
+#include <iostream>
+extern "C" {
+   void display_message(const char* message);
+   char read_input();
+
+
+}
 
 using namespace std;
 
+int main() {
+   display_message("Welcome to Blackjack!\n\n");
+   display_message("Press any key to start...\n");
+
+   char userInput = read_input();
 
 
-
-int main(){
-   asmMain();
    return 0;
+}
+
+void display_message(const char* message) {
+   cout << message;
+}
+
+char read_input() {
+   char input;
+   cin >> input;
+   return input;
 }
