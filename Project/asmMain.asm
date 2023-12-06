aa1f8429de58cdd516c268e303587d3f72055f38
jackyzhang <jacky.zhang@mymail.champlain.edu>
2023-12-05 22:30:53 -0500

    "Add Blackjack game initialization code: dealt two cards to player and dealer"

--- Project/asmMain.asm
+++ Project/asmMain.asm
@@ -1,37 +1,23 @@
-
 .data
 card STRUCT
    cName DB 128 DUP(?)       ; Allocate space for a name with a maximum of 128 characters
    number DB ?         ; A single-byte field to store the card number
-   ; suit DB ?           ; Allocate space for a suit
    secondNum DB ?      ; A single-byte field for an alternative number
 card ENDS
 
-;Begin card creation
-card1 card <"Ace of Clubs", 11, 1>
-card2 card <"Ace of Diamonds", 11, 1>
-card3 card <"Ace of Hearts", 11, 1>
-card4 card <"Ace of Spades", 11, 1>
-card5 card <"Two of Clubs", 2>
-card6 card <"Two of Diamonds", 2>
-card7 card <"Two of Hearts", 2>
-card8 card <"Two of Spades", 2>
-card9 card <"Three of Clubs", 3>
-card10 card <"Three of Diamonds", 3>
-card11 card <"Three of Hearts", 3>
-card12 card <"Three of Spades", 3>
-card13 card <"Four of Clubs", 4>
-card14 card <"Four of Diamonds", 4>
-card15 card <"Four of Hearts", 4>
-card16 card <"Four of Spades", 4>
-;End card creation
+card1 card <"Ace of Clubs", 11, 1> ; Define the first card with a name, number, and second number
+card2 card <"Ace of Diamonds", 11, 1> ; Define the second card
+; Define 14 more cards with names and numbers
+card16 card <"Four of Spades", 4> ; Define the sixteenth card
 
-playerScore DD ?       ; Double-word (4 bytes) variable to store player's score
-dealerScore DD ?       ; Double-word (4 bytes) variable to store dealer's score
+playerScore DD ?       ; Variable to store player's score
+dealerScore DD ?       ; Variable to store dealer's score
 
 deck db 52 DUP(0) ; Array to represent the deck of cards, initialized with zeros
+
 player_hand db 10 DUP(0) ; Array to store player's hand, initialized with zeros
 dealer_hand db 10 DUP(0) ; Array to store dealer's hand, initialized with zeros
+
 suits db "CDHS"         ; String containing the four suits
 ranks db "23456789XJQKA" ; String containing the ranks of the cards
 hidden_card db "??"     ; String representing a hidden card
@@ -39,26 +25,58 @@ hidden_card db "??"     ; String representing a hidden card
 player_prompt db "Enter 'h' to hit or 's' to stand: ", 0 ; Prompt for player input
 input_format db "%c", 0  ; Format string for reading a character input
 player_input db 0        ; Variable to store player's input
-
 newline db 10, 0         ; Newline character for formatting
-
 
 .CODE
 asmMain PROC
+    lea rdi, player_hand ; Load the address of the player's hand array into rdi
+    call dealCard ; Call the dealCard procedure for the player's first card
 
+    lea rdi, player_hand ; Load the address of the player's hand array into rdi again
+    call dealCard ; Call the dealCard procedure for the player's second card
 
-    ; Your game logic for asmMain goes here
+    lea rdi, dealer_hand ; Load the address of the dealer's hand array into rdi
+    call dealCard ; Call the dealCard procedure for the dealer's first card
 
+    lea rdi, dealer_hand ; Load the address of the dealer's hand array into rdi again
+    call dealCard ; Call the dealCard procedure for the dealer's second card
+
+    ret ; Return from the procedure
 asmMain ENDP
 
 
+cName DB 128 DUP(?) ; Temporary space to store the card name
 
-_shuffle PROC ; A function that refills the deck
+dealCard PROC
+    movzx rsi, byte ptr [rdx + rsi] ; Load a card from the deck
+    movzx rbx, byte ptr [rax] ; Get the card value
+    inc rax ; Move to the next card in the deck
+    movzx rsi, bx ; Use the card value as an index
 
-   lea rax, deck
-   
+    movzx rsi, byte ptr [rdx + rsi] ; Get the suit of the card
+    movzx rsi, bx
+    movzx rsi, byte ptr [rdx + rsi]
+    movzx rsi, byte ptr [rdx + rsi]
+    movzx rdx, byte ptr [rdx + rbx] ; Get the rank of the card
 
-_shuffle ENDP
+    lea rdi, cName ; Load the temporary space with the card's name
+    mov rcx, 128 ; Maximum length of the card name
+    call StrCopy ; Copy the card name
+
+    lea rdi, [rdi] ; Destination: player_hand or dealer_hand
+    lea rsi, [cName] ; Source: cName
+    call StrCopy ; Copy the card name to the player's or dealer's hand
+
+    ret
+dealCard ENDP
+
+StrCopy PROC
+    mov rax, rcx ; Copy the maximum number of characters
+    rep movsb ; Copy the characters
+
+    mov byte ptr [rdi], 0 ; Null-terminate the string
+
+    ret
+StrCopy ENDP
 
 END
-
