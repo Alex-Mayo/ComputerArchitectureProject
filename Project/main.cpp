5194419435933e28ea5907de87113bbea00a2185
jackyzhang <jacky.zhang@mymail.champlain.edu>
2023-12-05 20:43:58 -0500

    Grab code from main branch.

--- Project/main.cpp
+++ Project/main.cpp
@@ -1,29 +1,29 @@
 #include <iostream>
 extern "C" {
-    void display_message(const char* message);
-    char read_input();
-
+	void display_message(const char* message);
+	char read_input();
+	void asmMain();
 
 }
 
 using namespace std;
 
 int main() {
-    display_message("Welcome to Blackjack!\n\n");
-    display_message("Press any key to start...\n");
+	display_message("Welcome to Blackjack!\n\n");
+	display_message("Press any key to start...\n");
 
-    char userInput = read_input();
+	char userInput = read_input();
+	asmMain();
 
-
-    return 0;
+	return 0;
 }
 
 void display_message(const char* message) {
-    cout << message;
+	cout << message;
 }
 
 char read_input() {
-    char input;
-    cin >> input;
-    return input;
+	char input;
+	cin >> input;
+	return input;
 }
