2bd34dec5781e062dafb94f8f517ef986cc0160a
jackyzhang <jacky.zhang@mymail.champlain.edu>
2023-12-10 14:52:41 -0500

    Parts of starting game

--- Project/asmMain.asm
+++ Project/asmMain.asm
@@ -5,6 +5,8 @@ readPlayerCard PROTO
 readDealerCard PROTO
 movePlayerCard PROTO
 moveDealerCard PROTO
+extrn ReadConsoleA : PROC
+includelib user32.lib
 
 .data
 
@@ -22,8 +24,17 @@ player_input db 0        ; Variable to store player's input
 
 newline db 10, 0         ; Newline character for formatting
 
+input_buffer BYTE 10 DUP (?) ; Buffer to store user input
 
 .CODE
+getPlayerInput PROC
+    lea rdx, player_prompt  ; Load the address of the player prompt
+    lea rcx, input_buffer   ; Load the address of the input buffer
+    mov r8, 10              ; Maximum number of characters to read
+    call ReadConsoleA       ; Call ReadConsoleA to read a character input
+    movzx eax, byte ptr [input_buffer] ; Load the character input into eax
+    ret
+getPlayerInput ENDP
 asmMain PROC
 
    ; Your game logic for asmMain goes here
@@ -34,33 +45,53 @@ asmMain PROC
 asmMain ENDP
 
 _gameStart PROC ; deal two cards to player and dealer
+    shuffleCheck:
+        call getDeckSize
+        cmp rax, 52
+        jnbe noShuffle
+        call shuffle
 
-   shuffleCheck:
-      call getDeckSize
-      cmp rax, 52
-      jnbe noShuffle
-      call shuffle
+    noShuffle:
+        mov playerScore, 0
+        mov dealerScore, 0
+        mov playerHandSize, 0
+        mov dealerHandSize, 0 
 
-   noShuffle:
-   mov playerScore, 0
-   mov dealerScore, 0
-   mov playerHandSize, 0
-   mov dealerHandSize, 0 
+        call _dealPlayer
+        call _dealPlayer
+        add playerHandSize, 2
 
-   call _dealPlayer
-   pop rsp
-   call _dealPlayer
-   add playerHandSize, 2
+        call _dealDealer
+        call _dealDealer
+        add dealerHandSize, 2
 
-   call _dealDealer
-   call _dealDealer
-   add dealerHandSize, 2
+        call _getScore
 
-   call _getScore
-   ret
+    playerInputLoop:
+        ; Display player prompt and get input
+        call getPlayerInput
 
+        ; Check the input (assuming 'h' for hit, 's' for stand)
+        cmp al, 'h'
+        je hit
+        cmp al, 's'
+        je stand
+        jmp playerInputLoop ; Invalid input, loop again
+
+    hit:
+        ; Handle player hit logic (call a function, update scores, etc.)
+        ; ...
+
+        jmp afterPlayerTurn
+
+    stand:
+        ; Handle player stand logic (call a function, update scores, etc.)
+        ; ...
+
+    afterPlayerTurn:
+        ; Continue with the rest of the game logic
+        ret
 _gameStart ENDP
-
 _dealPlayer PROC
    call randomNumber
    mov rcx, rax
